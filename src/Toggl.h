b554fa34e1cba6e28d56fcaf3eec6065a2b3e395
Enrique Jorreto <ejorreto@gmail.com>
2025-03-27 21:32:16 +0100

    Updated Toggl API url to version v9 Updated Toggl root certificate Code cleanup to remove ESP8266 dependencies.

--- src/Toggl.h
+++ src/Toggl.h
@@ -1,8 +1,8 @@
 /*
- * Arduino ESP8266 & ESP32 Toggl API v8 Port
- * Using ArduinoJson Version 6
- * Using Toggl API v8
- * https://github.com/toggl/toggl_api_docs/blob/master/toggl_api.md
+ * Arduino ESP32 Toggl API v9 Port
+ * Using ArduinoJson Version 7
+ * Using Toggl API v9
+ * https://engineering.toggl.com/docs/
  */
 
 #ifndef TOGGL_H
@@ -11,100 +11,91 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-
-// Dependencies for ESP8266
-# if defined (ESP8266)
-
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClientSecureBearSSL.h>
-#include "base64.h"
-
 // Dependencies for ESP32
-# elif defined(ESP32)
-
 #include "WiFi.h"
 #include <HTTPClient.h>
 #include <base64.h>
 
-# else
-#error "Please define ESP processor, ESP32 or ESP8266"
+class Toggl
+{
+public:
+  Toggl();
 
-# endif
+  // Get the induvidual account settings/data
+  const uint16_t getID();
+  const String   getApiToken();
+  const uint16_t getDefaultWid();
+  const String   getEmail();
+  const String   getFullName();
+  const String   getJqTimeOfDayFormat();
+  const String   getJqDateFormat();
+  const String   getTimeOfDayFormat();
+  const String   getDateFormat();
+  const bool     getStoreStartAndStopTime();
+  const uint16_t getBeginningOfWeek();
+  const String   getLang();
+  const String   getDurationFormat();
+  const String   getAt();
+  const String   getCreation();
+  const String   getTimezone();
 
-class Toggl{
-  public:
-    Toggl();
+  // Misc
+  const String getWorkSpace();
+  const String getProject(int const & WID);
+  // const int       getPID(String const& WID ,String const& ProjectName);
+  const String CreateTag(String const & Name, int const & WID);
 
-    //Get the induvidual account settings/data
-    const uint16_t  getID();
-    const String    getApiToken();
-    const uint16_t  getDefaultWid();
-    const String    getEmail();
-    const String    getFullName();
-    const String    getJqTimeOfDayFormat();
-    const String    getJqDateFormat();
-    const String    getTimeOfDayFormat();
-    const String    getDateFormat();
-    const bool      getStoreStartAndStopTime();
-    const uint16_t  getBeginningOfWeek();
-    const String    getLang();
-    const String    getDurationFormat();
-    const String    getAt();
-    const String    getCreation();
-    const String    getTimezone();
+  // Timer related functions
+  const String  StartTimeEntry(String const & Description, String const & Tags, int const & PID, String const & CreatedWith);
+  const String  StopTimeEntry(String const & ID);
+  const String  CreateTimeEntry(String const & Description, String const & Tags, int const & Duration, String const & Start, int const & PID, String const & CreatedWith);
+  const int32_t getTimerDuration();
+  const String  getTimerID();
+  const bool    isTimerActive();
 
-    //Misc
-    const String    getWorkSpace();
-    const String    getProject(int const& WID);
-    //const int       getPID(String const& WID ,String const& ProjectName);
-    const String    CreateTag(String const& Name, int const& WID);
-    
-    //Timer related functions
-    const String    StartTimeEntry(String const& Description, String const& Tags, int const& PID, String const& CreatedWith);
-    const String    StopTimeEntry(String const& ID);
-    const String    CreateTimeEntry(String const& Description, String const& Tags, int const& Duration, String const& Start,  int const& PID, String const& CreatedWith);
-    const int32_t   getTimerDuration();
-    const String    getTimerID();
-    const bool      isTimerActive();
-    
-    //General functionality
-    void init(const char* SSID,const char* PASS);
-    void setAuth(String const& Token);
+  // General functionality
+  void init(const char * SSID, const char * PASS);
+  void setAuth(String const & Token);
 
-  private:
-    const uint32_t  getCurrentTime(const String Timezone);
-    const String    getUserData(String Input);
-    const String    getTimerData(String Input);
-    String          AuthorizationKey{};
-    const char*     Fingerprint{"41c40c6a907d364b26d40d40d24f0c1b42f126da"};  // Fingerprint valid until 22 April 2021
-	const char* root_ca = \
-	"-----BEGIN CERTIFICATE-----\n"\
-	"MIIESjCCAzKgAwIBAgINAeO0nXfN9AwGGRa24zANBgkqhkiG9w0BAQsFADBMMSAw\n"\
-	"HgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEGA1UEChMKR2xvYmFs\n"\
-	"U2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjAeFw0xNzA2MTUwMDAwNDJaFw0yMTEy\n"\
-	"MTUwMDAwNDJaMEIxCzAJBgNVBAYTAlVTMR4wHAYDVQQKExVHb29nbGUgVHJ1c3Qg\n"\
-	"U2VydmljZXMxEzARBgNVBAMTCkdUUyBDQSAxRDIwggEiMA0GCSqGSIb3DQEBAQUA\n"\
-	"A4IBDwAwggEKAoIBAQCy2Xvh4dc/HJFy//kQzYcVeXS3PkeLsmFV/Qw2xn53Qjqy\n"\
-	"+lJbC3GB1k3V6SskTSNeiytyXyFVtSnvRMvrglKrPiekkklBSt6o3THgPN9tek0t\n"\
-	"1m0JsA7jYfKy/pBsWnsQZEm0CzwI8up5DGymGolqVjKgKaIwgo+BUQzzornZdbki\n"\
-	"nicUukovLGNYh/FdEOZfkbu5W8xH4h51toyPzHVdVwXngsaEDnRyKss7VfVucOtm\n"\
-	"acMkuziTNZtoYS+b1q6md3J8cUhYMxCv6YCCHbUHQBv2PeyirUedtJQpNLOML80l\n"\
-	"A1g1wCWkVV/hswdWPcjQY7gg+4wdQyz4+anV7G+XAgMBAAGjggEzMIIBLzAOBgNV\n"\
-	"HQ8BAf8EBAMCAYYwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMBIGA1Ud\n"\
-	"EwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFLHdMl3otzdy0s5czib+R3niAQjpMB8G\n"\
-	"A1UdIwQYMBaAFJviB1dnHB7AagbeWbSaLd/cGYYuMDUGCCsGAQUFBwEBBCkwJzAl\n"\
-	"BggrBgEFBQcwAYYZaHR0cDovL29jc3AucGtpLmdvb2cvZ3NyMjAyBgNVHR8EKzAp\n"\
-	"MCegJaAjhiFodHRwOi8vY3JsLnBraS5nb29nL2dzcjIvZ3NyMi5jcmwwPwYDVR0g\n"\
-	"BDgwNjA0BgZngQwBAgEwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly9wa2kuZ29vZy9y\n"\
-	"ZXBvc2l0b3J5LzANBgkqhkiG9w0BAQsFAAOCAQEAcUrEwyOu9+OyAnmME+hTjoDF\n"\
-	"8OPvcWCpqXs0ZYU0vUc7A1cWAJlIOuDg8OrNtkg81aty8NAby2QtOw10aNd0iDF8\n"\
-	"aroO8IxNeM7aEPSKlkWXqZetxTUaGGTok7YNnR+5Xh2A6udbnI6uDqaE0tEXzrP7\n"\
-	"9oFPPOZon8/xpnbFfafz3X1YD+D2YQEcUY52MytInVyBUXIIF7r9AdPuRvn0smhA\n"\
-	"mTEBbE8bxlbrgXPSeVIFkiZbcc2dxNLOI3cPQXppXiElxvi3/3r3R97CAHucWkWc\n"\
-	"Kk5GkNl1LNj/jO7M3GnrbOYV0KP/SAusVd/fJZ1CtlGjZpVgxdAi5yJ6UaXMhw==\n"\
-	"-----END CERTIFICATE-----\n";
-	const String BaseUrl = "https://api.track.toggl.com/api/v8";
+private:
+  const uint32_t getCurrentTime(const String Timezone);
+  const String   getUserData(String Input);
+  const String   getTimerData(String Input);
+  String         AuthorizationKey{};
+  const char *   Fingerprint{"41c40c6a907d364b26d40d40d24f0c1b42f126da"}; // Fingerprint valid until 22 April 2021
+  const char *   root_ca =
+      "-----BEGIN CERTIFICATE-----\n"
+      "MIIFVzCCAz+gAwIBAgINAgPlk28xsBNJiGuiFzANBgkqhkiG9w0BAQwFADBHMQsw\n"
+      "CQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzEU\n"
+      "MBIGA1UEAxMLR1RTIFJvb3QgUjEwHhcNMTYwNjIyMDAwMDAwWhcNMzYwNjIyMDAw\n"
+      "MDAwWjBHMQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZp\n"
+      "Y2VzIExMQzEUMBIGA1UEAxMLR1RTIFJvb3QgUjEwggIiMA0GCSqGSIb3DQEBAQUA\n"
+      "A4ICDwAwggIKAoICAQC2EQKLHuOhd5s73L+UPreVp0A8of2C+X0yBoJx9vaMf/vo\n"
+      "27xqLpeXo4xL+Sv2sfnOhB2x+cWX3u+58qPpvBKJXqeqUqv4IyfLpLGcY9vXmX7w\n"
+      "Cl7raKb0xlpHDU0QM+NOsROjyBhsS+z8CZDfnWQpJSMHobTSPS5g4M/SCYe7zUjw\n"
+      "TcLCeoiKu7rPWRnWr4+wB7CeMfGCwcDfLqZtbBkOtdh+JhpFAz2weaSUKK0Pfybl\n"
+      "qAj+lug8aJRT7oM6iCsVlgmy4HqMLnXWnOunVmSPlk9orj2XwoSPwLxAwAtcvfaH\n"
+      "szVsrBhQf4TgTM2S0yDpM7xSma8ytSmzJSq0SPly4cpk9+aCEI3oncKKiPo4Zor8\n"
+      "Y/kB+Xj9e1x3+naH+uzfsQ55lVe0vSbv1gHR6xYKu44LtcXFilWr06zqkUspzBmk\n"
+      "MiVOKvFlRNACzqrOSbTqn3yDsEB750Orp2yjj32JgfpMpf/VjsPOS+C12LOORc92\n"
+      "wO1AK/1TD7Cn1TsNsYqiA94xrcx36m97PtbfkSIS5r762DL8EGMUUXLeXdYWk70p\n"
+      "aDPvOmbsB4om3xPXV2V4J95eSRQAogB/mqghtqmxlbCluQ0WEdrHbEg8QOB+DVrN\n"
+      "VjzRlwW5y0vtOUucxD/SVRNuJLDWcfr0wbrM7Rv1/oFB2ACYPTrIrnqYNxgFlQID\n"
+      "AQABo0IwQDAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4E\n"
+      "FgQU5K8rJnEaK0gnhS9SZizv8IkTcT4wDQYJKoZIhvcNAQEMBQADggIBAJ+qQibb\n"
+      "C5u+/x6Wki4+omVKapi6Ist9wTrYggoGxval3sBOh2Z5ofmmWJyq+bXmYOfg6LEe\n"
+      "QkEzCzc9zolwFcq1JKjPa7XSQCGYzyI0zzvFIoTgxQ6KfF2I5DUkzps+GlQebtuy\n"
+      "h6f88/qBVRRiClmpIgUxPoLW7ttXNLwzldMXG+gnoot7TiYaelpkttGsN/H9oPM4\n"
+      "7HLwEXWdyzRSjeZ2axfG34arJ45JK3VmgRAhpuo+9K4l/3wV3s6MJT/KYnAK9y8J\n"
+      "ZgfIPxz88NtFMN9iiMG1D53Dn0reWVlHxYciNuaCp+0KueIHoI17eko8cdLiA6Ef\n"
+      "MgfdG+RCzgwARWGAtQsgWSl4vflVy2PFPEz0tv/bal8xa5meLMFrUKTX5hgUvYU/\n"
+      "Z6tGn6D/Qqc6f1zLXbBwHSs09dR2CQzreExZBfMzQsNhFRAbd03OIozUhfJFfbdT\n"
+      "6u9AWpQKXCBfTkBdYiJ23//OYb2MI3jSNwLgjt7RETeJ9r/tSQdirpLsQBqvFAnZ\n"
+      "0E6yove+7u7Y/9waLd64NnHi/Hm3lCXRSHNboTXns5lndcEZOitHTtNCjv0xyBZm\n"
+      "2tIMPNuzjsmhDYAPexZ3FL//2wmUspO8IFgV6dtxQ/PeEMMA3KgqlbbC1j+Qa3bb\n"
+      "bP6MvPJwNQzcmRk13NfIRmPVNnGuV/u3gm3c\n"
+      "-----END CERTIFICATE-----\n";
+  const String BaseUrl = "https://api.track.toggl.com/api/v9";
 };
 
 #endif
